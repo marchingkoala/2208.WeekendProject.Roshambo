8d8a3dc019d7e63ede0499841e43d0deee078bc9
Yijin Ro <yri.carmine@gmail.com>
2022-10-22 18:57:57 -0400

    route to CreatePlayer added

--- client/App.js
+++ client/App.js
@@ -4,19 +4,25 @@ import { Link } from "react-router-dom";
 import Leaderboard from './components/Leaderboard';
 import Home from './components/Home'
 import SinglePlayer from './components/SinglePlayer';
+import CreatePlayer from './components/CreatePlayer';
 
 const App = () => {
 
-  const [players, setPlayers] = useState([])
-  const [selectedPlayer, setSelectedPlayer] = useState({})
-  const [doneLoading, setDoneLoading] = useState(false)
+  const [players, setPlayers] = useState([]) // collection of all players from /api/players
+  const [selectedPlayer, setSelectedPlayer] = useState({}) //information of 1 selected player from /api/plyaers/:playerId
+  const [doneLoading, setDoneLoading] = useState(false) // true/false state made specifically for rendering solution.
+  // with doneLoading state, we can safely map the selectedPlayer's game result
 
+  //getting datas of all players for Leaderboard page
+  //useEffect is triggered when user enters Leaderboard page
     const fetchingPlayers = async () => {
       const fetchedData = await fetch("/api/players");
       const json = await fetchedData.json();
       setPlayers(json);
     };
 
+  //getting data of 1 selected player for Singleplayer page
+  //this function is triggered when user clicks on player's name button
     const selectingPlayer = async (playerId) =>{
       const fetchingPlayer = await fetch(`/api/players/${playerId}`);
       const json = await fetchingPlayer.json()
@@ -47,6 +53,7 @@ const App = () => {
           />
         }
       ></Route>
+      <Route path="/create-player" element={<CreatePlayer />}></Route>
     </Routes>
   );
 };
