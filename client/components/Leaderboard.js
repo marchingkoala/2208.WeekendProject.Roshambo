5a94e7f1263b724b4fd23bf9ad25765ef751c22b
Yijin Ro <yri.carmine@gmail.com>
2022-10-28 17:31:42 -0400

    useDispatch used to fetch datas

--- client/components/Leaderboard.js
+++ client/components/Leaderboard.js
@@ -1,46 +1,50 @@
 import React, { useEffect } from 'react';
-import { Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllPlayers } from '../store/gameSlice';
 import { Link } from "react-router-dom";
-import SinglePlayer from './SinglePlayer';
+import axios from 'axios';
 
-const Leaderboard = ({
-  selectingPlayer,
-  fetchingPlayers,
-  players,
-  selectedPlayer,
-}) => {
-  
+const Leaderboard = ({selectingPlayer}) => {
+
+  // const [allPlayers, setAllPlayers] = useState([]);
+
+  // const fetchingPlayers = async () => {
+  //   const fetchedData = await fetch("/api/players");
+  //   const json = await fetchedData.json();
+  //   setAllPlayers(json);
+  // };
+
+  const dispatch = useDispatch();
+  const allPlayers = useSelector((state) => state.allPlayers);
+
+  const fetchingPlayers = async () => {
+    const datas = await axios.get("/api/players");
+    dispatch(setAllPlayers(datas));
+    console.log("HELLO EVERYONE!!!!!!!")
+    };
+
+
   useEffect(() => {
-    fetchingPlayers();
+    fetchPlayers();
   }, []);
+
 
   return (
     <div>
-      <h1>Look at these Brave Warriors!</h1>
-      <div className="warriorList">
-        {players.map((player) => {
+        {allPlayers.map((player) => {
           return (
             <div key={player.id}>
-              <div className="playerLinkButton">
                 <Link to={`/leaderboard/${player.id}`}>
-                  <button
-                    onClick={() => {
-                      selectingPlayer(player.id);
-                    }}
-                  >
+                  <button onClick={() => { selectingPlayer(player.id); }} >
                     <p>
-                      <b>Name: </b>
-                      {player.username}
-                    </p>
+                      <b>Name: </b> {player.username} </p>
                   </button>
                 </Link>
-              </div>
             </div>
           );
         })}
-      </div>
     </div>
   );
-};
+};;
 
 export default Leaderboard;
