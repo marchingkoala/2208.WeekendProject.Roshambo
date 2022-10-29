8b6918eae1f1ddbfd6d84f9d252c8ea47b98e75c
Yijin Ro <yri.carmine@gmail.com>
2022-10-29 15:34:21 -0400

    redux update removed old states

--- client/components/SinglePlayer.js
+++ client/components/SinglePlayer.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import {setSelectedPlayers } from '../store/gameSlice'
+import axios from 'axios';
 
-const SinglePlayer = ({ selectedPlayer, doneLoading }) => {
+const SinglePlayer = () => {
 
-  if (doneLoading === true) {
-      return (
+  const {id} = useParams() 
+
+  const dispatch = useDispatch();
+  const selectedPlayer = useSelector(state => state.game.selectedPlayer);
+  const [doneLoading, setDoneLoading] = useState(false);
+
+  const selectingPlayer = async () => {
+      const { data: selectedUser } = await axios.get(`/api/players/${id}`);
+      dispatch(setSelectedPlayers(selectedUser));
+      setDoneLoading(true);
+      };
+  
+  useEffect(()=>{
+    selectingPlayer();
+  }, [])
+
+  if (doneLoading) {
+    return (
+      <div>
+        <h2>Player Info</h2>
+        <p>
+          <b>Player UserName: </b>
+          {selectedPlayer.username}
+        </p>
         <div>
-          <h2>Player Info</h2>
-          <p>
-            <b>Player ID: </b>
-            {selectedPlayer.id}
-          </p>
-          <p>
-            <b>Player UserName: </b>
-            {selectedPlayer.username}
-          </p>
-          <div>
-            {selectedPlayer.games.map((game)=>{
-              return (
-                <p key={game.id}><b>Game {game.id}'s result was: </b>{game.result}</p>
-              )
-            })}
-          </div>
+          {selectedPlayer.games.map((game) => {
+            return (
+              <p key={game.id}>
+                <b>Game {game.id}'s result was: </b>
+                {game.result}
+              </p>
+            );
+          })}
         </div>
-      );
+      </div>
+    );
   }
 };
 
