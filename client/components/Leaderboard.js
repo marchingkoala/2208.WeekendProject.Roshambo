ccd2702a906c09ecccec51048fd93e97719baca3
Yijin Ro <yri.carmine@gmail.com>
2022-10-29 15:33:25 -0400

    onChange removed for cleaner code

--- client/components/Leaderboard.js
+++ client/components/Leaderboard.js
@@ -1,41 +1,31 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setAllPlayers } from '../store/gameSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { setAllPlayers } from "../store/gameSlice";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
-const Leaderboard = ({selectingPlayer}) => {
-
-  // const [allPlayers, setAllPlayers] = useState([]);
-
-  // const fetchingPlayers = async () => {
-  //   const fetchedData = await fetch("/api/players");
-  //   const json = await fetchedData.json();
-  //   setAllPlayers(json);
-  // };
-
+const Leaderboard = ({ selectingPlayer }) => {
+  const allPlayers = useSelector((state) => state.game.allPlayers);
   const dispatch = useDispatch();
-  const allPlayers = useSelector((state) => state.allPlayers);
 
   const fetchingPlayers = async () => {
-    const datas = await axios.get("/api/players");
-    dispatch(setAllPlayers(datas));
-    console.log("HELLO EVERYONE!!!!!!!")
-    };
+    const { data: test } = await axios.get("/api/players");
+    dispatch(setAllPlayers(test));
 
+  };
 
   useEffect(() => {
-    fetchPlayers();
+    fetchingPlayers();
   }, []);
-
 
   return (
     <div>
-        {allPlayers.map((player) => {
+      <h2>Hello World</h2>
+      {allPlayers.map((player) => {
           return (
             <div key={player.id}>
                 <Link to={`/leaderboard/${player.id}`}>
-                  <button onClick={() => { selectingPlayer(player.id); }} >
+                  <button >
                     <p>
                       <b>Name: </b> {player.username} </p>
                   </button>
