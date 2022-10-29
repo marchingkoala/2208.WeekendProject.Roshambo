89d906722dcbd7bdec45c4e694f22cd35253bb4d
Yijin Ro <yri.carmine@gmail.com>
2022-10-29 15:35:41 -0400

    browserrouter used instead of router

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
@@ -10,8 +10,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('app')
 );
 root.render(
-<Router>
-  <Provider store={store}>
-<App />
-  </Provider>
-</Router>);
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
